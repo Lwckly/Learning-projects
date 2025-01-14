e2404e77519b278f70cd5a0295310db55332c7d5
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-01-14 16:05:57 -0300

    Update ep3.py

--- Python/ep3.py
+++ Python/ep3.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#note:Not tool optimal, made following university assignment recomendations
+#note:Not tool optimal, made following university assignment recommendations
 #nota:Não otimizado com relação ao uso de ferramentas, feito com base nas regras de entrega da universidade
 
 def le_dados():
